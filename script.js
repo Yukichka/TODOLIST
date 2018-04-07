4156e5fef02767d49451d269e55fdb37ee4b21eb
Yukichka <yukiinsw@gmail.com>
2018-04-08 00:10:17 +0100

    dataModel WIP

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-      var n = 4
+var dataSet = []
+
+$( document ).ready(function () {
+    render();
+});
+
+function initialDataSet (){
+  return [ 
+    { id: 'm1', text: 'message1', timeStamp: new Date(), urgence:0 },
+    { id: 'm2', text: 'message2', timeStamp: new Date(), urgence:1 },
+    { id: 'm3', text: 'message3', timeStamp: new Date(), urgence:0 },
+    { id: 'm4', text: 'message4', timeStamp: new Date(), urgence:1 },
+  ];
+}
+
+dataSet = initialDataSet()
+
+function render (){
+  $('ul.message').text('');
+  dataSet.forEach(function(element){
+    var urgence = 'NOT AT ALL'
+    if (element.urgence ==0) urgence = 'NOT AT ALL'
+    if (element.urgence ==1) urgence = 'NORMAL'
+    if (element.urgence ==2) urgence = 'URGENT'
+      
+    $('ul.message').append(
+          '<li id="'+element.id+'"><p><span class="context">'+element.text
+         +'</span><b> '+urgence+'</b></p>'
+         +'<span>' 
+         + new Date().toLocaleDateString("en-GB",options)
+         +' '
+         + new Date().toLocaleTimeString("en-GB")
+         +' '
+         +'</span>'
+         +'<button onclick="changeMessage(\'m'+element.id+'\')" class="btn btn-outline-warning btn-sm">CHANGE</button>'
+         +' <button onclick="removeMessage(\'m'+element.id+'\')" class="btn btn-outline-danger btn-sm">REMOVE</button>' 
+         +'</li>');
+  
+
+  })
+}
+
+     
 
       $(document).keypress(function (e) {
         if (e.which == 13) {
@@ -32,11 +74,11 @@ function addMessage() {
            +' <button onclick="removeMessage(\'m'+n+'\')" class="btn btn-outline-danger btn-sm">REMOVE</button>' 
            +'</li>');
 
-  };  
+  }  
 
 function removeMessage(id){
       $('li#'+id).remove();
-    };
+    }
 
       
         
@@ -49,5 +91,5 @@ function changeMessage(id){
     if (newValue.length ===0) 
     return alert('Ooops! Please type somthing!');
     $('li#'+id+' .context').text(newValue);
-     };
-          
+     }
+        
